4fe1d1664056d92d623b64be3a543cb2e8f0e40c
Marlon O'Neil <hello@marlononeil.com>
2017-09-06 16:42:19 -0400

    commented out seeds file

--- Part2-Play/db/seeds.rb
+++ Part2-Play/db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Actor.create(name: "Dwayne Johnson", age: 48, skill: "Action Star")
-Show.create(name: "WWE wrestling", date: Time.now, time: Time.now, director_id: 1)
-Role.create(name: "The Rock", actor_id: 1, show_id: 1)
-Director.create(name: "Vince McMan")
+# Actor.create(name: "Dwayne Johnson", age: 48, skill: "Action Star")
+# Show.create(name: "WWE wrestling", date: Time.now, time: Time.now, director_id: 1)
+# Role.create(name: "The Rock", actor_id: 1, show_id: 1)
+# Director.create(name: "Vince McMan")
