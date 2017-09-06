e54d9988cec482123d172dc66750d592443f4e2a
Marlon O'Neil <hello@marlononeil.com>
2017-09-06 15:07:23 -0400

    Part1-People generated city model with province_id foreign key and migrated database

--- Part1-People/db/schema.rb
+++ Part1-People/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906190038) do
+ActiveRecord::Schema.define(version: 20170906190231) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.integer "year_founded"
+    t.integer "province_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
