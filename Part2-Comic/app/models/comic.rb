2fe7ba65d3f0a6b4b3df0091575be5bc7a203d5e
Marlon O'Neil <hello@marlononeil.com>
2017-09-10 11:52:06 -0400

    Completed associations and testing for models and join tables

--- Part2-Comic/app/models/comic.rb
+++ Part2-Comic/app/models/comic.rb
@@ -1,2 +1,3 @@
 class Comic < ApplicationRecord
+  has_many :issues
 end
--- Part2-Comic/app/models/issue.rb
+++ Part2-Comic/app/models/issue.rb
@@ -1,2 +1,5 @@
 class Issue < ApplicationRecord
+  belongs_to :comic
+  has_and_belongs_to_many :artists
+  has_and_belongs_to_many :writers
 end
